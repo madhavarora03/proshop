23c27c6eee8daf0ca27787671cbd265cd30a0ec2
madhav <eragon.cube@outlook.com>
2023-09-27 17:46:18 +0530

    setting up environment variable for port

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
 import products from './data/products.js'
 
-const port = 5000
+const port = process.env.PORT || 5000;
 
 const app = express()
 
