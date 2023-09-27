20bf7672fd86d83e30116a7d76875bc429130c91
madhav <eragon.cube@outlook.com>
2023-09-27 18:27:35 +0530

    Fetching each product page from backend, and removing products.js file

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import {
@@ -9,13 +10,20 @@ import {
   Button,
   ListGroupItem,
 } from 'react-bootstrap'
-import products from '../products'
 import Rating from '../components/Rating'
+import axios from 'axios'
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({})
   const { id: productId } = useParams()
-  const product = products.find((p) => p._id === productId)
-  console.log(product)
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`)
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [productId])
 
   return (
     <div>
