a89b0277e79db5810d604d4311f6ddd200acdf51
madhav <eragon.cube@outlook.com>
2023-10-04 11:21:04 +0530

    Setting up product controllers

---
+++ backend/controllers/productController.js
@@ -0,0 +1,25 @@
+import asyncHandler from '../middleware/asyncHandler.js'
+import Product from '../models/productModel.js'
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({})
+  res.json(products)
+})
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    return res.json(product)
+  } else {
+    res.status(404)
+    throw new Error('Resource not found')
+  }
+})
+
+export { getProducts, getProductById }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,28 +1,8 @@
 import express from 'express'
 const router = express.Router()
-import Product from '../models/productModel.js'
+import { getProducts, getProductById } from '../controllers/productController.js'
 
-import asyncHandler from '../middleware/asyncHandler.js'
-
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-  })
-)
-
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    if (product) {
-      return res.json(product)
-    } else {
-      res.status(404)
-      throw new Error('Resource not found')
-    }
-  })
-)
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router
