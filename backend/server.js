5a8379fe1b51bb26bde228e8ad09de2cea01aed5
madhav <eragon.cube@outlook.com>
2023-10-04 09:25:32 +0530

    fetching data from database

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
 
 const port = process.env.PORT || 5000
 
@@ -13,14 +13,7 @@ app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use('/api/products', productRoutes)
 
 app.listen(port, () => {
   console.log(`Server running on port http://localhost:${port}`)
