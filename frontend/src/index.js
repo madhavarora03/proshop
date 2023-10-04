4d39b0e233b523cf7ffe437e52f9f6f8b20f0007
madhav <eragon.cube@outlook.com>
2023-10-04 17:18:53 +0530

    setting up redux for state management

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/styles/index.css'
 import './assets/styles/bootstrap.custom.css'
@@ -26,7 +28,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 )
 
