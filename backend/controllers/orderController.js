51aef98e53861de8dab6abfbdeaf8994e11704a5
madhav <eragon.cube@outlook.com>
2023-11-07 22:48:45 +0530

    Adding PayPal payment method functionality

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -67,14 +67,32 @@ const getOrderById = asyncHandler(async (req, res) => {
 });
 
 // @desc    Update Order to paid
-// @route   GET /api/orders/:id/pay
+// @route   PUT /api/orders/:id/pay
 // @access  Private
 const updateOrderToPay = asyncHandler(async (req, res) => {
-  res.send('updateOrderToPay');
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    res.status(200).json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
 });
 
 // @desc    Update Order to delivered
-// @route   GET /api/orders/:id/deliver
+// @route   PUT /api/orders/:id/deliver
 // @access  Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
   res.send('updateOrderToDelivered');
--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,39 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
-import connectDB from './config/db.js'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
-import cookieParser from 'cookie-parser'
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import cookieParser from 'cookie-parser';
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-connectDB()
-const app = express()
+connectDB();
+const app = express();
 
 // Body Parser Middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // cookie parser middleware
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
-  res.send('API is running...')
-})
+  res.send('API is running...');
+});
 
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/orders', orderRoutes)
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.get('/api/config/paypal', (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID }),
+);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log(`Server running on port http://localhost:${port}`)
-})
+  console.log(`Server running on port http://localhost:${port}`);
+});
