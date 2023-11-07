ef0c7c39b8109c5e64a6a60b446e8fe58ce02420
madhav <eragon.cube@outlook.com>
2023-11-07 21:38:54 +0530

    Remove admins only access for getOrderById route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -12,7 +12,7 @@ import { protect, admin } from '../middleware/authMiddleware.js';
 router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
 
 router.route('/myorders').get(protect, getMyOrders);
-router.route('/:id').get(protect, admin, getOrderById);
+router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPay);
 router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
 
