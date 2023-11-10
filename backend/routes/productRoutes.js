9ab2ed3c27ddba88c2df558998bd2a879bafde1a
madhav <eragon.cube@outlook.com>
2023-11-11 03:36:25 +0530

    Add createProductReview function to productController and productRoutes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ import {
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
@@ -15,5 +16,7 @@ router
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
+
+router.route('/:id/reviews').post(protect, createProductReview);
 
 export default router;
