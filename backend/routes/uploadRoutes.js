217c1a5724c85de0276aaa945fc12304a30f17a0
madhav <eragon.cube@outlook.com>
2023-11-10 17:18:43 +0530

    Adding checkFileType while uploading image

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -10,7 +10,7 @@ const storage = multer.diskStorage({
   filename(req, file, cb) {
     cb(
       null,
-      `${file.fieldname}-${Date.now()}.${path.extname(file.originalname)}`,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`,
     );
   },
 });
@@ -25,6 +25,9 @@ function checkFileType(file, cb) {
 
 const upload = multer({
   storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
 });
 
 router.post('/', upload.single('image'), (req, res) => {
