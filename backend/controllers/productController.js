251d4c18cba06604ccfe9c1104452f0685df1427
madhav <eragon.cube@outlook.com>
2023-11-11 21:12:18 +0530

    Add pagination limit to productController.js and update .env.example

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,7 @@ import Product from '../models/productModel.js';
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 8;
+  const pageSize = process.env.PAGINATION_LIMIT;
   const page = Number(req.query.pageNumber) || 1;
 
   const keyword = req.query.keyword
