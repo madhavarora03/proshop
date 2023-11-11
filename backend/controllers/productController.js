f13657587efce50056e8f1191437d8a3f6f6eb66
madhav <eragon.cube@outlook.com>
2023-11-11 10:34:33 +0530

    Update getProductById response status to 200

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -26,7 +26,7 @@ const getProducts = asyncHandler(async (req, res) => {
 const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
   if (product) {
-    return res.json(product);
+    return res.status(200).json(product);
   } else {
     res.status(404);
     throw new Error('Resource not found');
